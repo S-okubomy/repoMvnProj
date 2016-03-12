dd6a45ba3d2061888229a116eb2406c21fd1b8e1
S-okubomy <okubo1504@gmail.com>
2016-03-12 23:10:51 +0900

    ブランチ追加後 PG 修正
    printout 追加

--- hajiboot/src/main/java/com/example/App.java
+++ hajiboot/src/main/java/com/example/App.java
@@ -12,7 +12,10 @@ public class App
         System.out.println( "Hello World! spring" );
         System.out.println( "Spring Hello World!" );
         System.out.println( "Spring Hello World!2" );
+<<<<<<< 3c371f879eec4496d8f0590d8406b1a4d6c9ecd4
         System.out.println( "Spring Hello World!123123" );
+=======
+>>>>>>> ブランチ追加後 PG 修正
         System.out.println( "Spring Hello World!3" );
     }
 }
