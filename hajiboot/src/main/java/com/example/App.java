db016187e2e8cdca99312f496ab484e0d3dd8ede
S-okubomy <okubo1504@gmail.com>
2016-03-12 23:10:51 +0900

    ブランチ追加後 PG 修正
    printout 追加

--- hajiboot/src/main/java/com/example/App.java
+++ hajiboot/src/main/java/com/example/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Spring Hello World!" );
+        System.out.println( "Spring Hello World!2" );
+        System.out.println( "Spring Hello World!3" );
     }
 }
