5a0b7a2ed110187aac638f87bc8d3b9c0651cebf
S-okubomy <okubo1504@gmail.com>
2016-03-13 01:50:25 +0900

    test2 競合

--- hajiboot/src/main/java/com/example/App.java
+++ hajiboot/src/main/java/com/example/App.java
@@ -12,14 +12,5 @@ public class App
         System.out.println( "Hello World! spring" );
         System.out.println( "Spring Hello World!" );
         System.out.println( "Spring Hello World!2" );
-<<<<<<< dd6a45ba3d2061888229a116eb2406c21fd1b8e1
-<<<<<<< 3c371f879eec4496d8f0590d8406b1a4d6c9ecd4
-        System.out.println( "Spring Hello World!123123" );
-=======
->>>>>>> ブランチ追加後 PG 修正
-        System.out.println( "Spring Hello World!3" );
-=======
-        System.out.println( "Spring Hello World!3123" );
->>>>>>> test 競合
     }
 }
