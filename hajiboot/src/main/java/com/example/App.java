3c371f879eec4496d8f0590d8406b1a4d6c9ecd4
S-okubomy <okubo1504@gmail.com>
2016-03-12 23:10:51 +0900

    ブランチ追加後 PG 修正
    printout 追加

--- hajiboot/src/main/java/com/example/App.java
+++ hajiboot/src/main/java/com/example/App.java
@@ -13,5 +13,6 @@ public class App
         System.out.println( "Spring Hello World!" );
         System.out.println( "Spring Hello World!2" );
         System.out.println( "Spring Hello World!123123" );
+        System.out.println( "Spring Hello World!3" );
     }
 }
